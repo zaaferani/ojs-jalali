e3c132c44080bf07ae5703d23f42749f7b1e2e5a
Hassan <zaaferani.arta@gmail.com>
2023-10-02 12:20:21 +0330

    en to fa done

--- en2fa.py
+++ en2fa.py
@@ -1,1 +1,16 @@
 import polib
+
+files = ["admin","author","default","editor","emails","locale","manager","manager","submission"]
+
+for file in files:
+    fa_pofile = polib.pofile(f'locale/fa/{file}.po')
+    en_pofile = polib.pofile(f'locale/en/{file}.po')
+
+    for entry in fa_pofile:
+        if entry.msgstr == "":
+            en_found = en_pofile.find(entry.msgid)
+            # print(entry.msgid, en_found.msgstr if en_found else None)
+            if en_found:
+                entry.msgstr = en_found.msgstr
+    fa_pofile.save()
+    print(f'file {file} done')
